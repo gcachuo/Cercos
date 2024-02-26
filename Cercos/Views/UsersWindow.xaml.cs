0e89477904f8fb7afead65132a793ef3b12c33aa
Memo Cachú <gcachu.o@gmail.com>
2024-02-25 19:52:17 -0600

    Save users to database

--- Cercos/Views/UsersWindow.xaml.cs
+++ Cercos/Views/UsersWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using Cercos.Extensions;
+using Cercos.Services;
+using Cercos.Tools;
 
 namespace Cercos.Views
 {
@@ -13,6 +16,33 @@ namespace Cercos.Views
         private void BtnReturn_OnClick(object sender, RoutedEventArgs e)
         {
             this.Navigate<HomeWindow>();
+        }
+
+        private void BtnGuardar_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fullname = FullName.Text;
+            var password = Password.Text;
+
+            if (!Validation.ValidateFields(new List<string> { fullname, password }))
+            {
+                MessageBox.Show("Llene todos los campos");
+                return;
+            }
+
+            if (UsersService.CheckDuplicates(password))
+            {
+                MessageBox.Show("Eliga otra clave.");
+                return;
+            }
+
+            var passwordHashed = PasswordHasher.HashPassword(password);
+            
+            UsersService.Insert(fullname, passwordHashed);
+
+            FullName.Text = "";
+            Password.Text = "";
+
+            MessageBox.Show("Guardado correctamente.");
         }
     }
 }
